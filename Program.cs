132c2ce67fb69da2503ccd251736fcb678158933
Pao-Esquinca <pl836507@gmail.com>
2023-10-05 18:24:46 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L.S.C_3K_Ejer03_Cgato
+{
+    using System;
+    class Program
+    {
+        static void Main()
+        {
+            int[] numeros = { 10, 20, 30, 40, 50, 60 };
+            Console.WriteLine(+numeros[3]);
+
+            int[] n = new int[] { 3, 4, 5 };
+            Console.WriteLine(+n[1]);
+
+            char[] c = { 'L', 'u', 'i', 's' };
+            Console.WriteLine(c);
+
+            string s = "Mona lisa";
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
